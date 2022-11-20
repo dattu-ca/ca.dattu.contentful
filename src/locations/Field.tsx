20fe5628b0f5672130095b533bf5d595d32ebeb2
Dattu Patel <dattu.patel@gmail.com>
2022-11-20 15:14:55 -0500

    [#183789118] Added Youtube Extractor

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -4,21 +4,23 @@ import {FieldExtensionSDK} from "@contentful/app-sdk";
 import { /* useCMA, */ useSDK} from "@contentful/react-apps-toolkit";
 import TextToJsonEditorComponent from "../custom/components/textToJsonEditor";
 import LinksEditorComponent from "../custom/components/linksEditor";
-
+import YoutubeExtractorComponent from "../custom/components/youtubeExtractor";
 
 
 const Field = () => {
     const location = window.location.href.split("/").pop() as string;
     const sdk = useSDK<FieldExtensionSDK>();
     useEffect(() => sdk.window.startAutoResizer(), [sdk.window]);
-
-
+    
     switch (location) {
         case "text-to-json-editor": {
             return <TextToJsonEditorComponent/>;
         }
         case "links-editor": {
             return <LinksEditorComponent/>;
+        }
+        case "youtube-extractor": {
+            return <YoutubeExtractorComponent/>;
         }
         default: {
             return <Paragraph>Did not yet implement (AppId: {sdk.ids.app}), location: {location}</Paragraph>;
